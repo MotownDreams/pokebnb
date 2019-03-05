f82316d49decf2521ddd28509d35f153181b41e4
Ferdinand Nussbaum <ferdinand18.2n@gmail.com>
2019-03-05 12:54:05 +0100

    Add validations 3

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -10,7 +10,7 @@ class Booking < ApplicationRecord
   def end_date_after_start_date
     return if end_date.blank? || start_date.blank?
 
-    if end_date < start_date
+    if end_date <= start_date
       errors.add(:end_date, "must be after the start date")
     end
   end
