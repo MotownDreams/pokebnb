c05dc3e0ac29f737657471e174eeb7a8f0f6ee11
Darrick Ervin II <sherwood_3494@yahoo.com>
2019-03-05 11:56:51 +0100

    flat index

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,15 @@
 class FlatsController < ApplicationController
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
+  end
 end
