17ff5cbfcebc15781be176e1cb3c8cabf3d6103a
Ferdinand Nussbaum <ferdinand18.2n@gmail.com>
2019-03-06 13:14:04 +0100

    Photo Uploader is done

--- db/migrate/20190306104153_create_photos.rb
+++ db/migrate/20190306104153_create_photos.rb
@@ -1,7 +1,8 @@
 class CreatePhotos < ActiveRecord::Migration[5.2]
   def change
     create_table :photos do |t|
-
+      t.references :flat, foreign_key: true
+      t.string :photo
       t.timestamps
     end
   end
--- db/migrate/20190306104158_add_photo_to_photos.rb
+++
@@ -1,5 +0,0 @@
-class AddPhotoToPhotos < ActiveRecord::Migration[5.2]
-  def change
-    add_column :photos, :photo, :string
-  end
-end
