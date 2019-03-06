c2b8f5f2220fb4cee406e395d8cb7dc310ba0537
Darrick Ervin II <sherwood_3494@yahoo.com>
2019-03-06 11:58:56 +0100

    initialization complete

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -4,4 +4,9 @@ class Flat < ApplicationRecord
   validates :location, presence: true
   validates :price_per_night, presence: true, numericality: { only_integer: true }
   validates :description, presence: true
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
 end
+
