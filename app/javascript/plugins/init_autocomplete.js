7b3496c570bc6ff2cc8cfee22035bf92aa1037f1
Einar Jonsson <einarjonsson@mac.com>
2019-03-07 22:54:45 +0100

    changed order of initAutocomplete method to increase speed

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -1,13 +1,12 @@
 const initAutocomplete = (selector) => {
-  const places = require('places.js');
   const element = document.getElementById(selector)
   if (element) {
+    const places = require('places.js');
     const searchAutocomplete = places({
       appId: 'pl9D490WAV0U',
       apiKey: '2059e9302af6d4cbeff9b9cd7fe86f62',
       container: element
     });
-    console.log(`initialized ${selector}`)
   }
 }
 
