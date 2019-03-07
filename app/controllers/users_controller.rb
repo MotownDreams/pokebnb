f43fa1c68da7f0b7d16f8f04dd40422385157eec
Ferdinand Nussbaum <ferdinand18.2n@gmail.com>
2019-03-07 15:55:50 +0100

    Not even close

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+  def show
+    @flat = Flat.find(params[:id])
+    redirect_to user_path
+  end
+
+  # def edit
+  #   @flat = Flat.find(params[:id])
+  # end
 end
