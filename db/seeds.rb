7c71c2681201153857fb49a661aef660c5998a06
Einar Jonsson <einarjonsson@mac.com>
2019-03-08 13:13:02 +0100

    made the home search align vertically to the center

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Booking.destroy_all
 Flat.destroy_all
 User.destroy_all
 
