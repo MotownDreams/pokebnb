b6155cafcb643457dbef2f2494646eab9b1f87c2
Ferdinand Nussbaum <ferdinand18.2n@gmail.com>
2019-03-08 15:12:41 +0100

    Validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,14 +4,25 @@ class Booking < ApplicationRecord
 
   validates :state, presence: true
   validates :total_price, presence: true
-  validates :start_date, :end_date, presence: true
-  validate :end_date_after_start_date
+  validate :end_after_start
+  validates :start_date, :end_date, :presence => true
+  validate :trip_before_today
 
-  def end_date_after_start_date
+  private
+
+  def end_after_start
     return if end_date.blank? || start_date.blank?
 
-    if end_date <= start_date
+    if end_date < start_date
       errors.add(:end_date, "must be after the start date")
+    end
+  end
+
+  def trip_before_today
+    return if end_date.blank? || start_date.blank?
+
+    if Time.now >= end_date && start_date
+      errors.add(:start_date, "must be at least today")
     end
   end
 end
